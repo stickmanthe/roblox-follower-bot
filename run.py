9828131ca60055d67dc52f86a1c19d85d35915e8
stickmanthe <89025036+stickmanthe@users.noreply.github.com>
2023-10-06 22:12:16 +0300

    Update run.py
    The code has become less messy

--- run.py
+++ run.py
@@ -1,59 +1,33 @@
 from selenium import webdriver
-
 from selenium.webdriver.common.by import By
-
 import time
 
-
- 
 print("ru: Загрузка Браузера... ")
 print("en: Starting Browser... ")
 
-choto = webdriver.Firefox()
-# For Chrome/Для Хром или Яндекс Браузера: choto = webdriver.Chrome()
-# For Safari/Для Сафари: choto = webdriver.Safari()
+browser = webdriver.Firefox()
+# For Chrome: browser = webdriver.Chrome()
+# For Safari: browser = webdriver.Safari()
+# For Firefox: browser = webdriver.Firefox()
 
-
-
-print("ru: Входим на аккаунт...")
 print("en: Loggining... ")
+browser.get("https://roblox.com/login")
+username = choto.find_element(By.ID, "login-username")
+password = choto.find_element(By.ID, "login-password")
 
-
-choto.get("https://roblox.com/login")
-
-
-nik = choto.find_element(By.ID, "login-username")
-parol = choto.find_element(By.ID, "login-password")
-
-
-nik.send_keys("") # Nickname/Никнейм
-parol.send_keys("") # Password/Пароль
-
-
+username.send_keys("") # Type Roblox Account Nickname here
+password.send_keys("") # Type Roblox Account Password here
 
 voiti = choto.find_element(By.ID, "login-button")
-
-
-
 voiti.click()
-
-
-
 time.sleep(10)
-
-
 x = 7
 y = 8
-
-
 x += 3
-
-
 while x >= y:
  f = open("ostalos.txt", "r+")
  prosm = int(f.read())
- print("ru: Подписка на айди... ")
- print("en: Following... ")
+ print("Following... ")
  choto.get(f"https://roblox.com/users/{prosm}")
  tritochki = choto.find_element(By.ID, "popover-link")
  tritochki.click()
@@ -65,7 +39,6 @@ while x >= y:
  f.truncate(0)
  f.seek(0)
  f.write(af)
- print("ru: УСПЕШНО ПОДПИСАНО! ПЕРЕХОД НА АЙДИ: " + af )
- print("en: FOLLOWED! JUMPING TO ID: " + af )
+ print("FOLLOWED! JUMPING TO ID: " + af )
  f.close()
  time.sleep(3)
