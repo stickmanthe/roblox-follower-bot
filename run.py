ac4a8db08a17defebdf1430fe7032420551eff32
stickmanthe <89025036+stickmanthe@users.noreply.github.com>
2024-03-31 15:17:51 +0300

    New features & updated for modern Roblox

--- run.py
+++ run.py
@@ -2,37 +2,49 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-print("Starting Browser... ")
 
-browser = webdriver.Firefox()
-# For Chrome: browser = webdriver.Chrome()
-# For Safari: browser = webdriver.Safari()
-# For Firefox: browser = webdriver.Firefox()
+putBrowser = input("Please enter the number to choose your browser (1 is Chrome, 2 is Safari, 3 is Firefox): ")
+putUsername = input("Type your Roblox Account username here: ")
+putPassword = input("Type Roblox Account Password here: ")
 
-print("en: Loggining... ")
+match putBrowser:
+ case "1":
+  print("Ok. Starting Chrome... ")
+  browser = webdriver.Chrome()
+ case "2":
+  print("Ok. Starting Safari... ")
+  browser = webdriver.Safari()
+ case "3":
+  print("Ok. Starting Firefox... ")
+  browser = webdriver.Firefox()
+ case _:
+  print("Unknown browser!")
+  exit()
+
+print("Loggining in... ")
 browser.get("https://roblox.com/login")
 username = browser.find_element(By.ID, "login-username")
 password = browser.find_element(By.ID, "login-password")
 
-username.send_keys("") # Type Roblox Account Nickname here
-password.send_keys("") # Type Roblox Account Password here
+username.send_keys(putUsername)
+password.send_keys(putPassword)
 
-voiti = browser.find_element(By.ID, "login-button")
-voiti.click()
-time.sleep(10)
+loginBtn = browser.find_element(By.ID, "login-button")
+loginBtn.click()
+time.sleep(10)
 
 while True:
  f = open("id.txt", "r+")
- prosm = int(f.read())
+ idRead = int(f.read())
  print("Following... ")
- browser.get(f"https://roblox.com/users/{prosm}")
- tritochki = browser.find_element(By.ID, "popover-link")
- tritochki.click()
+ browser.get(f"https://roblox.com/users/{idRead}")
+ userOptions = browser.find_element(By.ID, "popover-link")
+ userOptions.click()
  time.sleep(2)
- podpis = browser.find_element(By.ID, "profile-follow-user")
- podpis.click()
+ followUser = browser.find_element(By.LINK_TEXT, "Follow")
+ followUser.click()
  time.sleep(2)
- af = str(prosm - 1)
+ af = str(idRead - 1)
  f.truncate(0)
  f.seek(0)
  f.write(af)
